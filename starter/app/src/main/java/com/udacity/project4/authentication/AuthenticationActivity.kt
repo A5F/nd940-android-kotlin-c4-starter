9c6d0afff4e5a800edb93ec734822a0be65b1d3b
Andrea <andreaferri46@gmail.com>
2022-03-16 23:21:27 +0100

    init firebaseUI

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,16 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.udacity.project4.R
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,15 +18,66 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+
+    val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        val firebaseAuth = FirebaseAuth.getInstance()
+        // If the user was authenticated, send him to RemindersActivity
+        if (firebaseAuth.currentUser != null) {
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+            startActivity(Intent(this, RemindersActivity::class.java))
+            finish()
+        }else {
+//         Implement the create account and sign in using FirebaseUI,
+    //          use sign in using email and sign in using Google
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+            findViewById<View>(R.id.login_button).setOnClickListener {
+                doLogin()
+            }
+        }
+
+//
+
+
 
     }
+
+    fun doLogin(){
+        //a bonus is to customize the sign in flow to look nice using :
+        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        AuthUI.getInstance().createSignInIntentBuilder()
+            .setIsSmartLockEnabled(false)
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.map)
+            .setTheme(R.style.LoginScreenTheme)
+            .build()
+
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
 }
